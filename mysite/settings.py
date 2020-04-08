ac88b366132cff91276ef06459a98a07e564b281
Atishay990 <atishay990@gmail.com>
2020-04-09 02:06:18 +0530

    litmus is new admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,8 +143,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #)
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'atishay990@gmail.com'
-EMAIL_HOST_PASSWORD = 'technoware'
+EMAIL_HOST_USER = 'litmus.info5@gmail.com'
+EMAIL_HOST_PASSWORD = 'Litmus@123'
 EMAIL_PORT = 587
 
 #LOGIN_URL = 'litmus/login/'
